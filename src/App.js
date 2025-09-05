4ae5de5069827da6549cce55d791481d3b794e75
Geoffrey Kwan <geoffreykwan@gmail.com>
2025-09-04 20:59:01 -0400

    feat(counter): Implement main feature
    Displays for integer values
    Displays for binary values
    Buttons for incrementing
    Buttons for decrementing
    Input for manual number input
    Buttons to automatically set to 255 or 0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    value: 0,
+    binary: [0, 0, 0, 0, 0, 0, 0, 0],
+  };
+  setOctetValue = (newValue) => {
+    this.setState((prevState) => {
+      const newBinary = this.updateBinary(newValue);
+      return { value: newValue, binary: newBinary };
+    });
+  };
+  increaseOctetValue = (amount) => {
+    this.setState((prevState) => {
+      const newValue = this.validateValue(prevState.value + amount);
+      const newBinary = this.updateBinary(newValue);
+      return { value: newValue, binary: newBinary };
+    });
+  };
+  decreaseOctetValue = (amount) => {
+    this.setState((prevState) => {
+      const newValue = this.validateValue(prevState.value - amount);
+      const newBinary = this.updateBinary(newValue);
+      return { value: newValue, binary: newBinary };
+    });
+  };
+  validateValue = (newValue) => {
+    if (newValue >= 256) {
+      newValue = newValue % 256;
+    }
+    if (newValue < 0) {
+      newValue = 256 + (newValue % 256);
+    }
+    return newValue;
+  };
+  updateBinary = (newValue) => {
+    let binaryString = (newValue >>> 0).toString(2);
+    binaryString = binaryString.padStart(8, "0");
+    const newBinary = [0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < 8; i++) {
+      let intValue = parseInt(binaryString.charAt(i));
+      newBinary[i] = intValue ? intValue : 0;
+    }
+    return newBinary;
+  };
+  changeValueManually = (e) => {
+    let newValue = e.target.value;
+    if (newValue < 0) {
+      newValue = 0;
+    } else if (newValue >= 256) {
+      newValue = 255;
+    }
+    this.setState((prevState) => {
+      const newBinary = this.updateBinary(newValue);
+      return { value: newValue, binary: newBinary };
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="binary-positions">
+          {[...Array(8)].map((e, i) => (
+            <div>
+              <div className="integer-container border">
+                {Math.pow(2, 7 - i)}
+              </div>
+              <button
+                className="button increment-button"
+                style={{ margin: "12px", padding: "12px" }}
+                onClick={() => {
+                  this.increaseOctetValue(Math.pow(2, 7 - i));
+                }}
+              >
+                +
+              </button>
+              <div className="binary-container border">
+                {this.state.binary[i]}
+              </div>
+              <button
+                className="button increment-button"
+                style={{ margin: "12px", padding: "12px" }}
+                onClick={() => {
+                  this.decreaseOctetValue(Math.pow(2, 7 - i));
+                }}
+              >
+                -
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="value-input-container">
+          <div>
+            <button
+              className="button set-button"
+              onClick={() => this.setOctetValue(255)}
+            >
+              255
+            </button>
+          </div>
+          <div>
+            <input
+              className="value-input"
+              value={this.state.value}
+              onChange={this.changeValueManually}
+            />
+          </div>
+          <div>
+            <button
+              className="button set-button"
+              onClick={() => this.setOctetValue(0)}
+            >
+              0
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
